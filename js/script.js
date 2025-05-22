3e5915fb7b72794a693bca7a0f3205fc0c762a6f
PRAHASITH VUDDAGIRI <141656316+SAIPRAHASITH@users.noreply.github.com>
2025-05-22 10:43:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ function loadProductDetails() {
                     <h1>${product.name}</h1>
                     <p>${product.description}</p>
                     <h3>$${product.price.toFixed(2)}</h3>
-                    <button onclick="addToCart(${product.id}); add_to_cart(${product.id})" class="btn" >Add to Cart</button>
+                    <button onclick="addToCart(${product.id}); add_to_cart(${product.id})" class="btn"  id="add_to_cart">Add to Cart</button>
                 </div>
             `;
         } else {
