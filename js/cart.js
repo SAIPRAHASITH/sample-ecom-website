5701a7d08067899ef6f33c495ebc3f47505b7236
PRAHASITH VUDDAGIRI <141656316+SAIPRAHASITH@users.noreply.github.com>
2025-05-23 19:59:32 +0530

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -21,4 +21,4 @@ document.addEventListener("DOMContentLoaded", () => {
     cartItems.innerHTML = cartHTML;
     document.getElementById("cart-total").textContent = total.toFixed(2);
     updateCartCount();
-  );
+  )
